91b6c0e10ec7f2480a0812585f5cc427fd4bd12d
bipinmsit <bipin.kumar@aspecscire.com>
2020-07-24 08:05:41 +0530

    Updates

---
+++ precache-manifest.33d99d84be844dd23db104aef1027c90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6be4848fc8df86de8bafdac818be4e34",
+    "url": "/google_keep/index.html"
+  },
+  {
+    "revision": "4991eb140f3a0bb36d05",
+    "url": "/google_keep/static/css/main.32940649.chunk.css"
+  },
+  {
+    "revision": "554ca955edaab6803d7e",
+    "url": "/google_keep/static/js/2.5c260b46.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/google_keep/static/js/2.5c260b46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4991eb140f3a0bb36d05",
+    "url": "/google_keep/static/js/main.f72c61e4.chunk.js"
+  },
+  {
+    "revision": "92154d33ec9b4b9a8bcb",
+    "url": "/google_keep/static/js/runtime-main.f4b9260c.js"
+  }
+]);
